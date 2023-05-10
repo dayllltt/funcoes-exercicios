11f5ab010fd9367a83920a4ee04bd8a59baa0a71
Daylton Araujo <dayltongg46@gmail.com>
2023-05-09 22:41:04 -0300

    exercicio1/script.js

---
+++ exercicio1/script.js
@@ -0,0 +1,28 @@
+//function imprimirNome(nome) {
+  //  console.log(`Olá ${nome}!`)
+//}
+//
+//imprimirNome(`Daylton`)
+
+//function tabuada(numero1,numero2){
+//    console.log(`O resultado dessa multiplicação é ${numero1 * numero2}`)
+//}
+//invocando a função com numeros diferentes agora
+//tabuada(2, 6)
+//tabuada(5, 4)
+//tabuada(7, 8)
+//tabuada(9, 9)
+//tabuada(2, 5)
+//tabuada(2, 2)
+//tabuada(1, 1)
+//
+const calculoTabuada = (numero1, numero2) => {
+    const tabuada = numero1 * numero2
+    console.log(` O resultado desta multiplicação é: ${tabuada}`)
+}
+calculoTabuada(3,6)
+calculoTabuada(3,5)
+calculoTabuada(3,2)
+calculoTabuada(3,7)
+calculoTabuada(3,9)
+calculoTabuada(4,6)
